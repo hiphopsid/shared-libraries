5c246abdf0d7da624a10ee30ee6d63a17dd42550
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-05-04 09:08:52 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,8 +3,19 @@ def call(branch,ssh){
             userRemoteConfigs: [[credentialsId: 'b4f7364e-adde-475e-b024-2d20667e0987', url: ssh ]]])
 }
 
+// def buildImage(image) {
+//   echo "${image}"
+// }
+
 def buildImage(image) {
-  echo "${image}"
+   withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'docker_pass', usernameVariable: 'docker_user')]) {
+     sh "docker build -t ${image} ."
+     sh "docker login -u ${docker_user} -p ${docker_pass}"
+   sh "docker push ${image}"
+     sh "docker build -t ${image} ."
+     sh "docker login -u ${docker_user} -p ${docker_pass}"
+   sh "docker push ${image}"
+}
 }
 
 
