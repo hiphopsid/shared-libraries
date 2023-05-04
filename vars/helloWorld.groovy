a3901954f2b6b7faebe720ab052aa8c38ad89a95
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-05-04 08:10:01 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,7 +3,7 @@
 // userRemoteConfigs: [[credentialsId: 'admingithub', url: '${ssh}']]])
 // }
 def call(branch,ssh){
-  checkout([$class: 'GitSCM', branches: [[name: '*/${branch}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg:  [], \
-            userRemoteConfigs: [[credentialsId: 'b4f7364e-adde-475e-b024-2d20667e0987', url: '${ssh}']]])
+  checkout([$class: 'GitSCM', branches: [[name: branch ]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg:  [], \
+            userRemoteConfigs: [[credentialsId: 'b4f7364e-adde-475e-b024-2d20667e0987', url: ssh ]]])
 }
 
