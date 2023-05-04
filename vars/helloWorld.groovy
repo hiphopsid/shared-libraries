a7fa3948da7a4c671f6e65ed7e2a0421e9a8f5d3
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-05-04 09:01:44 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,7 +3,7 @@ def call(branch,ssh){
             userRemoteConfigs: [[credentialsId: 'b4f7364e-adde-475e-b024-2d20667e0987', url: ssh ]]])
 }
 
-def buildImage(image) {
+def build(image) {
   sh "echo ${image}"
 }
 
