27657115fdbdec49dfa40e4af4bfa87939928f8a
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-06-02 12:57:20 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -7,12 +7,12 @@ def call(branch,ssh){
 //   echo "${image}"
 // }
 
-def buildImage(String image) {
+def buildImage(String image, String application_name) {
    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'docker_user', passwordVariable: 'docker_pass')]) {
     bat """
     docker image build -f Dockerfile -t my-app-image .
     docker login -u ${docker_user} -p ${docker_pass}"
-    docker push hiphopsid/todo-app
+    docker push hiphopsid/${application_name}
     """
      echo "${image}"
 //      sh "docker build -t ${image} ."
