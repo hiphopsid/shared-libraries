e79de53cbfa44f687872790e8f42c30fd268036e
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-06-21 11:04:18 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
 def call(String image) {
    // withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'docker_user', passwordVariable: 'docker_pass')]) {
     bat """
-    docker image build -f Dockerfile -t ${image} .
+    docker image build -f Dockerfile -t ${image}:$BUILD_NUMBER .
     """
 //      sh "docker build -t ${image} ."
 //      sh "docker login -u ${docker_user} -p ${docker_pass}"
