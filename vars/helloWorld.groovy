5588c1bba2951c89ddd941ccfd4519e089d5da04
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-06-21 09:58:29 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,29 +1,29 @@
 def call(branch,ssh){
   checkout([$class: 'GitSCM', branches: [[name: branch ]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg:  [], \
-            userRemoteConfigs: [[credentialsId: 'b4f7364e-adde-475e-b024-2d20667e0987', url: ssh ]]])
+            userRemoteConfigs: [[credentialsId: 'hiphopsid', url: ssh ]]])
 }
 
 // def buildImage(image) {
 //   echo "${image}"
 // }
 
-def buildImage(String image, String application_name) {
-   withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'docker_user', passwordVariable: 'docker_pass')]) {
-    bat """
-    docker image build -f Dockerfile -t my-app-image .
-    docker login -u ${docker_user} -p ${docker_pass}"
-    docker push hiphopsid/${application_name}
-    """
-     echo "${image}"
-//      sh "docker build -t ${image} ."
-//      sh "docker login -u ${docker_user} -p ${docker_pass}"
-//    sh "docker push ${image}"
-}
-}
-def deployment(){
-     withKubeConfig(credentialsId: 'new-kubeconfig', namespace: '') {
-      bat 'kubectl apply -f Deployment-beta.yaml -n dev'
-    }
-}
+// def buildImage(String image, String application_name) {
+//    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'docker_user', passwordVariable: 'docker_pass')]) {
+//     bat """
+//     docker image build -f Dockerfile -t my-app-image .
+//     docker login -u ${docker_user} -p ${docker_pass}"
+//     docker push hiphopsid/${application_name}
+//     """
+//      echo "${image}"
+// //      sh "docker build -t ${image} ."
+// //      sh "docker login -u ${docker_user} -p ${docker_pass}"
+// //    sh "docker push ${image}"
+// }
+// }
+// def deployment(){
+//      withKubeConfig(credentialsId: 'new-kubeconfig', namespace: '') {
+//       bat 'kubectl apply -f Deployment-beta.yaml -n dev'
+//     }
+// }
 
 
