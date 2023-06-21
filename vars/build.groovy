0ea4e4a53a00b1f540899faaf156ea02e2423065
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-06-21 10:54:39 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,7 +3,6 @@ def buildImage(String image) {
     bat """
     docker image build -f Dockerfile -t ${image} .
     """
-     echo "${image}"
 //      sh "docker build -t ${image} ."
 //      sh "docker login -u ${docker_user} -p ${docker_pass}"
 //    sh "docker push ${image}"
