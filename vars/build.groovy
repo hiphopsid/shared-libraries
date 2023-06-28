db1e981a5744b02396e32a5e6cfe51d41915f9d3
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-06-28 17:03:43 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,13 +1,13 @@
 def call(String image, String branch) {
    // withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'docker_user', passwordVariable: 'docker_pass')]) {
-   if (branch == "master") {
+   if (branch == "beta" || branch=="dev") {
     bat """
     docker image build -f Dockerfile -t ${image}:$BUILD_NUMBER .
     """
    }
    else
    {
-    println "Please use master branch for docker build instead of " + branch
+    error "Please use beta/dev branch for non-prod docker builds instead of " + branch
    }
 //      sh "docker build -t ${image} ."
 //      sh "docker login -u ${docker_user} -p ${docker_pass}"
