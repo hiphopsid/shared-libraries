af7f6a6346645ade868450d8617ce4ae82f3b2a3
hiphopsid <54412380+hiphopsid@users.noreply.github.com>
2023-06-28 16:29:46 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,8 +1,14 @@
-def call(String image) {
+def call(String image, String branch) {
    // withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'docker_user', passwordVariable: 'docker_pass')]) {
+   if (branch == "master") {
     bat """
     docker image build -f Dockerfile -t ${image}:$BUILD_NUMBER .
     """
+   }
+   else
+   {
+    printl "Please use master branch for docker build instead of " + branch
+   }
 //      sh "docker build -t ${image} ."
 //      sh "docker login -u ${docker_user} -p ${docker_pass}"
 //    sh "docker push ${image}"
